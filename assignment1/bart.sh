d9df759babb14b726004f895d95f6ab34116a02f
PaulSun256 <paulsun700@gmail.com>
2020-09-18 18:06:06 -0400

    stopped window from closing instantly

--- assignment1/bart.sh
+++ assignment1/bart.sh
@@ -1,4 +1,6 @@
 javac bart.java
 java Bart
+
+# keeps window open till a key is pressed
 echo
 read -rsn1 -p "Press any key to continue..."
--- assignment2/question1/Money1b.sh
+++ assignment2/question1/Money1b.sh
@@ -1,4 +1,6 @@
 javac Money1a.java
 java Money
+
+# keeps window open till a key is pressed
 echo
 read -rsn1 -p "Press any key to continue..."
