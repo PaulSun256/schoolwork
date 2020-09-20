ff03185a4ab7a2b8ffecdbfdecb48f83778f4e1c
PaulSun256 <paulsun700@gmail.com>
2020-09-20 10:12:51 -0400

    added comments

--- assignment2/question3/Decimal2.java
+++ assignment2/question3/Decimal2.java
@@ -14,8 +14,12 @@
 
 class Equations	{
     public static void main(String[] args) {
+
+        // calculate the result of the equations, and rounds them to the nearest 100th
         float equation1 = (float)(Math.round((1 / 4.0 + 10 - 3 / 5.0) * 100.0) / 100.0);
         float equation2 = (float)(Math.round((Math.pow((27.3 + 0.3), 3) - (3 + 15.3) / (1.22 / 2)) / Math.sqrt(6) * 100.0) / 100.0);
+
+        // outputs our answer
         System.out.println(equation1);
         System.out.println(equation2);
     }
