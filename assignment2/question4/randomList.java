e3f41292350b586bfb5a01b3662651de6f959283
PaulSun256 <paulsun700@gmail.com>
2020-09-20 10:41:56 -0400

    added comments

---
+++ assignment2/question4/randomList.java
@@ -0,0 +1,28 @@
+/*
++========================================+
+|             Assignment 2 4             |
++========================================+
+|          Paul Sun, 2020/09/20          |
++========================================+
+|               ICS3U Ms.S               |
++========================================+
+|  Code that prints out a random element |
+|   of 3 different sets of integers. I   |
+|  didn't use arrays, I just configured  |
+| the random generator a bit differently |
+|              for each one.             |
++========================================+
+*/
+
+class RandomList	{
+    public static void main(String[] args) {
+        System.out.println(randInt(1, 30));
+        System.out.println(randInt(-4, 2));
+        System.out.println(randInt(1, 6) * 10);
+    }
+
+    public static int randInt(int min, int max) {
+        int randomInt = (int)(Math.random()*((max + 1) - min) + min);
+        return randomInt;
+    }
+}
