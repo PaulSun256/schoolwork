a90bd6901ee9e7afbb22f21832df535eec09a176
Sun <Paul.Sun2@student.tdsb.on.ca>
2020-09-30 08:44:56 -0400

    capitalized everything because why the hell not

--- submissions/Zip.bat
+++ submissions/Zip.bat
@@ -3,14 +3,14 @@ SET /P ID="what assignment to zip: "
 jar -cfM Sun_Paul_Assignment%ID%.zip ../Sun_Paul/src/Sun_Paul_Assignment%ID%
 
 REM CHECK IF FILE EXISTS
-IF EXIST Sun_Paul_Assignment%ID%.zip GOTO fileExists 
-GOTO oops
-:fileExists
+IF EXIST Sun_Paul_Assignment%ID%.zip GOTO FILEEXISTS 
+GOTO OOPS
+:FILEEXISTS
 ECHO assignment successfully zipped
-GOTO end
-:oops
+GOTO END
+:OOPS
 ECHO something went wrong oops lol
-:end
+:END
 
 REM STOPS WINDOW FROM IMMEDIATELY CLOSING
-pause
+PAUSE
